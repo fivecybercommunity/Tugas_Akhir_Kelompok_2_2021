6e6c55922535852f5229a4334e743b262fd47430
Qois-69912 <qois.firosi@gmail.com>
2021-11-17 14:42:10 +0700

    Page karya - Improvement & Responsive

--- scripts/website.js
+++ scripts/website.js
@@ -1,30 +1,22 @@
-let image = 1;
-let keterangan = [
-    'Francisco',
-    'Razi',
-    'Syafigya',
-    'Qois'
-]
+let portofolio = 1;
 
 function action(panah) {
-    // Remove current
-    document.getElementById('portofolio-' + image).style.display = 'none';
+    // Remove current portofolio
+    document.getElementById('portofolio-' + portofolio).style.display = 'none';
 
     // Calculation
     if (panah === 'kiri' ) {
-        image--;
-        if (image <= 0) {
-            image = 4;
+        portofolio--;
+        if (portofolio <= 0) {
+            portofolio = 4;
         }
     } else if (panah) {
-        image++;
-        if (image > 4) {
-            image = 1;
+        portofolio++;
+        if (portofolio > 4) {
+            portofolio = 1;
         }
     }
 
     // Display new image
-    document.getElementById('portofolio-' + image).style.display = 'block';
-
-    document.getElementById('keterangan-portofolio').innerHTML = 'Portofolio ' + keterangan[image - 1];
+    document.getElementById('portofolio-' + portofolio).style.display = 'flex';
 }
